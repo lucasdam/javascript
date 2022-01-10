867e64f2c1721798b9e42c08c7513c1a7d7b2b59
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-01-10 18:08:54 -0300

    Adicionado os exercícios das aulas 11 e 12 sobre Condições

---
+++ meus-exercicios/aula11_condicoes/ex010.js
@@ -0,0 +1,9 @@
+var velocidade = 78
+
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+if (velocidade > 60) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitira. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ meus-exercicios/aula11_condicoes/ex011.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro! Que pena...')
+} else {
+    console.log('Você é Estrangeiro!')
+}
