f76d33bb27dd85cf2d03e59ab7b4dbb200d0a40a
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-01-11 15:41:32 -0300

    Adicionado os exercícios das aulas 13 e 14 sobre Repetições

---
+++ meus-exercicios/aula13_while-dowhile/ex019.js
@@ -0,0 +1,13 @@
+// WHILE (Executa enquanto a condição for verdadeira)
+var cont = 1
+while (cont <= 3) {
+    console.log(`Usando WHILE ${cont}`)
+    cont++
+}
+
+// DO WHILE (Executa no mínimo 1x, independente se a condição for true ou false)
+cont = 1
+do {
+    console.log(`Usando DO WHILE ${cont}`)
+    cont++
+} while (cont <= 3)
---
+++ meus-exercicios/aula14_for-debugging/ex020.js
@@ -0,0 +1,16 @@
+console.log('Vai começar...')
+
+for (var c = 1; c <= 4; c++) {
+    console.log(`Passo ${c}`)
+}
+
+console.log('Fim!')
+
+/* DEBUGGING
+Marcar a linha que deseja dar início (BREAKPOINT)
+F5 (START) para iniciar o debugging
+Observar as variáveis em VARIABLES ou adicioná-las e observá-las em WATCH
+F10 (STEP OVER) para executar passo a passo
+Acompanhar o status das variáveis e saídas no console
+F5 (STOP) para encerrar
+*/
